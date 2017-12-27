4d5a357021005b75c700c244fb909a70ffc8e8b5
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-27 17:18:37 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
   end
 
   get '/account' do
@@ -32,7 +36,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username], :password => params[:password])
+    if user
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
   end
 
   get "/success" do
