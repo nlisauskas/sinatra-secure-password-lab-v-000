9c02ff3cc380310c0a6ef8722f9bded000d7df2e
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-27 17:34:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(:username => params[:username])
-    if user && user.autenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/account"
     else
